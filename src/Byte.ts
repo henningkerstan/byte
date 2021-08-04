17fcd51b75b2658199a9d2d41df1be8e8ab4871e
Henning Kerstan <mail@henningkerstan.de>
2021-08-04 16:20:03 +0200

    initial setup

---
+++ src/Byte.ts
@@ -0,0 +1,168 @@
+// Copyright 2020 Henning Kerstan
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * A class implementing a representation of a Byte (Octet).
+ */
+export class Byte {
+  /** The internal representation (LSB0 bit ordering).
+   *
+   *  Internally, this class uses bitshifts for getting/setting single bits. For this, JavaScript transforms the operands (numbers) to 32bit signed integers, applies the operation and then returns the obtained number.
+   */
+  private int8 = 0
+
+  /**
+   * Compute unsigned integer value with least significant bit first.
+   * @param begin Bit offset to be interpreted as LSB.
+   * @param size Size in bits; default is all remaining bits (8 - begin); must be >= 0 and satisfy (begin + size) <= 8.
+   */
+  public readUIntLSB(begin = 0, size: number = 8 - begin): number {
+    if (begin + size > 8) {
+      throw new Error('Out of bounds (begin + size exceeds 8)')
+    }
+
+    let val = 0
+    for (let i = begin; i < begin + size; i++) {
+      val += this.getBit(i) << (i - begin)
+    }
+    return val
+  }
+
+  /**
+   * Set unsigned integer value with least significant bit first.
+   * @param begin Bit offset to be interpreted as LSB.
+   * @param size Bit offset to be interpreted as MSB.
+   */
+  public writeUIntLSB(
+    value: number,
+    begin = 0,
+    size: number = 8 - begin,
+  ): void {
+    if (value < 0) {
+      throw new Error('Value out of bounds (< 0)')
+    }
+
+    if (size > 8) {
+      throw new Error('Size may not exceed 8 bits')
+    }
+
+    if (begin + size > 8) {
+      throw new Error('Out of bounds (begin + size exceeds 8)')
+    }
+
+    // determine max size
+    const max = (1 << size) - 1
+
+    if (value > max) {
+      throw new Error('Out of bounds (> ' + max + ')')
+    }
+
+    for (let i = begin; i < begin + size; i++) {
+      const bit = (value & (1 << (i - begin))) === 0 ? 0 : 1
+      this.setBit(i, bit)
+    }
+  }
+
+  /**
+   * Compute unsigned integer value with most significant bit first.
+   * @param begin Bit offset to be interpreted as MSB.
+   * @param end Bit offset to be interpreted as LSB.
+   */
+  public uIntMSB(begin = 0, end = 7): number {
+    // compute big endian representation
+    let val = 0
+    for (let i = begin; i <= end; i++) {
+      val += this.getBit(i) << (end - i)
+    }
+
+    return val
+  }
+
+  /**
+   * Construct a byte based on given numeric value.
+   * @param val Initial value (number will be interpreted as LSB0); default is 0.
+   */
+  constructor(val = 0) {
+    this.int8 = val
+  }
+
+  /**
+   * Get the bit at the supplied offset.
+   * @param offset The position (integer from 0 to 7).
+   */
+  getBit(offset: number): number {
+    return (this.int8 & (1 << offset)) === 0 ? 0 : 1
+  }
+
+  /**
+   * Set the bit at the supplied offset.
+   * @param offset The position (integer from 0 to 7).
+   * @param value Either 0 or 1, default value is 1.
+   */
+  setBit(offset: number, value = 1): void {
+    switch (value) {
+      case 1:
+        this.int8 = this.int8 | (1 << offset)
+        break
+
+      case 0:
+        this.clearBit(offset)
+        break
+
+      default:
+        throw new Error('bit value must be 0 or 1')
+    }
+  }
+
+  /**
+   * Clears the bit at the given offset (i.e. sets it to 0).
+   * @param offset Bit to clear (i.e. to set to 0).
+   */
+  clearBit(offset: number): void {
+    this.int8 = this.int8 & ~(1 << offset)
+  }
+
+  toString(): string {
+    let s = 'Byte {\n'
+    s += '  binary: '
+    for (let i = 1; i <= 8; i++) {
+      s += this.getBit(i - 1).toString()
+      if (i % 2 === 0) {
+        s += ' '
+      }
+    }
+
+    s += '\n'
+    s += '  uIntLSB(): ' + this.readUIntLSB().toString()
+    s += ' (0x' + this.readUIntLSB().toString(16).toUpperCase() + ')'
+    s += ', '
+    s += '  uIntMSB(): ' + this.uIntMSB().toString()
+    s += ' (0x' + this.uIntMSB().toString(16).toUpperCase() + ')\n'
+    s += '}'
+
+    return s
+  }
+
+  toBinaryString(): string {
+    let s = ''
+    for (let i = 1; i <= 8; i++) {
+      s += this.getBit(i - 1).toString()
+      if (i % 2 === 0) {
+        s += ' '
+      }
+    }
+
+    return s
+  }
+}
---
+++ tests/Byte.test.ts
@@ -0,0 +1,72 @@
+// Copyright 2020 Henning Kerstan
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// class test for Byte class
+import { Byte } from '../src/Byte'
+
+describe('A Byte', () => {
+  it('shall satisfy the invariant (new Byte(i)).uIntLSB() == i', () => {
+    for (let i = 0; i < 255; i++) {
+      const byte = new Byte(i)
+      expect(byte.readUIntLSB()).toBe(i)
+    }
+  })
+
+  it('shall satisfy "byte.writeUIntLSB(i) => byte.readUIntLSB() == i"', () => {
+    const byte = new Byte(0)
+    for (let i = 0; i < 255; i++) {
+      byte.writeUIntLSB(i)
+      expect(byte.readUIntLSB()).toBe(i)
+    }
+  })
+
+  it('shall ...', () => {
+    //
+  })
+
+  // // let byte = new Byte(123)
+  // // console.log(byte.toString())
+
+  // // console.log('uIntLSB(0, 3) = ' + byte.readUIntLSB(0, 4)) // 11
+  // // console.log('uIntLSB(1, 3) = ' + byte.readUIntLSB(1, 3)) // 5
+
+  // // console.log('uIntMSB(0, 3) = ' + byte.uIntMSB(0, 4)) // 13
+  // // console.log('uIntMSB(1, 3) = ' + byte.uIntMSB(1, 3)) // 5
+
+  // // byte = new Byte(0)
+  // // byte.writeUIntLSB(10, 1, 4)
+  // // console.log(byte.toString())
+
+  // for (let size = 0; size < 9; size++) {
+  //   const max = (1 << size) - 1
+
+  //   console.log('Testing size: ' + size + ' bits; range: 0-' + max)
+
+  //   for (let begin = 0; begin + size < 9; begin++) {
+  //     byte = new Byte(0)
+  //     console.log('  Beginning at offset ' + begin)
+
+  //     for (let val = 0; val <= max; val++) {
+  //       byte.writeUIntLSB(val, begin, size)
+  //       const readValue = byte.readUIntLSB(begin, size)
+  //       let msg = '    Wrote value ' + val
+  //       msg += '; read value ' + readValue
+  //       msg += '; binary: ' + byte.toBinaryString()
+  //       msg += val == readValue ? ' OK' : ' FAIL'
+  //       console.log(msg)
+  //     }
+  //   }
+  // }
+  // })
+})
